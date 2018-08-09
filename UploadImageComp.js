7fda9d326d1ca25bbc6487ec09be3029d1a87893
biko-the-bird <34891228+biko-the-bird@users.noreply.github.com>
2018-08-09 13:53:35 -0400

    Update UploadImageComp.js

--- UploadImageComp.js
+++ UploadImageComp.js
@@ -3,13 +3,25 @@ import React from 'react'
 class UploadImageComp extends React.Component {
 
     render() {
-        return (
-            <div>
-            <input style={{display: 'none'}} type="file" accept="image/*" 
-            onChange = {this.props.fileHandler} ref={fileInput => this.fileInput = fileInput}/>
-            <div onClick={() => {this.fileInput.click()}} style={{backgroundColor: this.props.bgColor, color: 'white', borderStyle: 'dotted', borderColor: 'white', height: '50px'}}><p style={{margin: '5px'}}>📂<br/>Select Profile Picture</p></div>
-          </div>
-        )
+
+        if (this.props.fileName) {
+            return (
+                <div>
+                <input style={{display: 'none'}} type="file" accept="image/*" 
+                onChange = {this.props.fileHandler} ref={fileInput => this.fileInput = fileInput}/>
+                <div onClick={() => {this.fileInput.click()}} style={{backgroundColor: this.props.bgColor, color: 'white', borderStyle: 'dotted', borderColor: 'white', height: '50px'}}><p style={{margin: '5px'}}>📂<br/>{this.props.fileName}</p></div>
+              </div>
+            )
+        } else {
+            return (
+                <div>
+                <input style={{display: 'none'}} type="file" accept="image/*" 
+                onChange = {this.props.fileHandler} ref={fileInput => this.fileInput = fileInput}/>
+                <div onClick={() => {this.fileInput.click()}} style={{backgroundColor: this.props.bgColor, color: 'white', borderStyle: 'dotted', borderColor: 'white', height: '50px'}}><p style={{margin: '5px'}}>📂<br/>Select Profile Picture</p></div>
+              </div>
+            )
+        }
+        
     }
 }
 export default UploadImageComp;
